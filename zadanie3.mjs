0e1141072c724b9a126b22225288820488920811
wiktor <wiktor.piszczako@gmail.com>
2024-11-01 13:30:07 +0100

    aktualizacja zadanie3.js

--- zadanie3.mjs
+++ zadanie3.mjs
@@ -15,4 +15,25 @@ http.createServer((req, res) => {
         res.end("Brak parametru 'file' w zapytaniu.");
         return;
     }
-})
+    const filePath = path.join(__dirname, fileName);
+
+    fs.stat(filePath, (err, stats) => {
+        if (err || !stats.isFile()) {
+            res.writeHead(404, { "Content-Type": "text/plain; charset=utf-8" });
+            res.end("Plik nie istnieje.");
+            return;
+        }
+
+        res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+        
+        const readStream = fs.createReadStream(filePath);
+        readStream.pipe(res);
+
+        readStream.on('error', (error) => {
+            res.writeHead(500, { "Content-Type": "text/plain; charset=utf-8" });
+            res.end("Wystąpil bląd podczas odczytu pliku.");
+        });
+    });
+}).listen(PORT, () => {
+    console.log(`Serwer nasluchuje na http://localhost:${PORT}`);
+});
